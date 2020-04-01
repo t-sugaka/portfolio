23eb7b1c51b20b9406aa8e023c5719e1bad23e1b
t-sugaka <sugaka.job@gmail.com>
2020-04-01 23:03:26 +0900

    ぐるなびAPIで検索できるようになった

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,18 +1,44 @@
 class RestaurantsController < ApplicationController
+	require "pry"
+
 	def index
+		p params
+	end
+	
+	def create
+
 	end
 	
 	def search
 		@prefs = pref_lists
 	end
 	
+	def result
+		# binding.pry
+		base_url='https://api.gnavi.co.jp/RestSearchAPI/v3'
+      
+	    parameters = {
+	      'keyid' => 'b35ede964272d8030ce8cbf69c942bb8',
+		  'category_l' => 'RSFST16000',
+		  'freeword' => params[:search],
+	      'pref' => 'PREF13',
+	      'hit_per_page' => 10
+	    }
+	      
+	    uri = URI(base_url + '?' + parameters.to_param)
+	      
+	    response_json = Net::HTTP.get(uri)
+	      
+	    p response_data = JSON.parse(response_json)
+	end
+
 	private
 	  
 	  def pref_lists
 		base_url = 'https://api.gnavi.co.jp/master/PrefSearchAPI/v3/'
 			
 		parameters = {
-		  'keyid' => '282619479f7052e7c6a53ddb3dde2f25',
+		  'keyid' => 'b35ede964272d8030ce8cbf69c942bb8',
 		  'lang' =>'ja'
 		}
 			
@@ -22,13 +48,28 @@ class RestaurantsController < ApplicationController
 		response_data = JSON.parse(response_json)
 		
 		# p response_data
-	  prefs = response_data['pref']
-	  pref_names = []
-	  prefs.each do |prefecture_data|
-		pref_names << prefecture_data["pref_name"]
-	  end
+		prefs = response_data['pref']
+		pref_names = []
+		prefs.each do |prefecture_data|
+			pref_names << prefecture_data["pref_name"]
+		end
 	
 	  p pref_names
 	  # prefs = prefs.each{|id,(pref_code, pref_name)| puts "#{id}: #{pref_code}, #{pref_name}"}
 	  end
+
+	  def search_params
+		base_url = 'https://api.gnavi.co.jp/master/PrefSearchAPI/v3/?keyid='
+
+		keyid = 'b35ede964272d8030ce8cbf69c942bb8'
+		parameters = {
+		  'lang' =>'ja',
+		  'freeword' => params[:freeword],
+		  'category_l' => 'RSFST16000',
+		#   'hit_per_page' => 10
+		}
+		 uri = URI(base_url + keyid + parameters.to_param)
+        response_json = Net::HTTP.get(uri)
+      	response_data = JSON.parse(response_json)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "restaurants#search"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  resources :restaurants
 
+  resources :restaurants
+  post 'restaurants/result', to: 'restaurants#result'
 end
